7f8f1ff6264d098fbc499c2dfb9b80c39fbad8dc
onemiles <690299233@qq.com>
2026-07-02 21:56:25 +0800

    Broaden stop-mode CAN wake edges

--- board/sys/power_saving.h
+++ board/sys/power_saving.h
@@ -101,15 +101,21 @@ static void enter_stop_mode(void) {
   // EXTI for CAN wakeup
   // EXTI8:  FDCAN1 RX (PB8)
   // EXTI5:  FDCAN2 RX (PB5)
-  // EXTI12: FDCAN3 RX (PD12)
+  // EXTI12: FDCAN2 alt RX (PB12) or FDCAN3 RX (PD12)
   set_gpio_mode(GPIOB, 8, MODE_INPUT);
   register_set(&(SYSCFG->EXTICR[2]), SYSCFG_EXTICR3_EXTI8_PB, 0xFU);
   set_gpio_mode(GPIOB, 5, MODE_INPUT);
   register_set(&(SYSCFG->EXTICR[1]), SYSCFG_EXTICR2_EXTI5_PB, 0xF0U);
-  set_gpio_mode(GPIOD, 12, MODE_INPUT);
-  register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PD, 0xFU);
+  if (harness.status == HARNESS_STATUS_FLIPPED) {
+    set_gpio_mode(GPIOD, 12, MODE_INPUT);
+    register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PD, 0xFU);
+  } else {
+    set_gpio_mode(GPIOB, 12, MODE_INPUT);
+    register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PB, 0xFU);
+  }
   uint32_t can_exti_line = (1UL << 8) | (1UL << 5) | (1UL << 12);
   register_set_bits(&(EXTI->IMR1), can_exti_line);
+  register_set_bits(&(EXTI->RTSR1), can_exti_line);
   register_set_bits(&(EXTI->FTSR1), can_exti_line);
 
   // clear pending EXTI
