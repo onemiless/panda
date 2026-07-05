5c6a01f8321814136ccbe7df9705618407527c0f
onemiles <690299233@qq.com>
2026-07-05 21:34:18 +0800

    Limit CAN wake monitor to panda builds

--- board/drivers/fdcan.h
+++ board/drivers/fdcan.h
@@ -222,11 +222,13 @@ void can_rx(uint8_t can_number) {
     safety_rx_invalid += safety_rx_hook(&to_push) ? 0U : 1U;
     ignition_can_hook(&to_push);
 
+    #ifndef PANDA_BODY
     if (wake_monitor_enabled && !wake_monitor_can_wake_requested) {
       wake_debug_stage(0x34U);
       bootkick_request_reset_pulse();
       wake_monitor_can_wake_requested = true;
     }
+    #endif
 
     led_set(LED_BLUE, true);
     rx_buffer_overflow += can_push(&can_rx_q, &to_push) ? 0U : 1U;
