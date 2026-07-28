6e1f2e0c2f08e5201b7857416dcf9bd31dcff9f6
onemiles <690299233@qq.com>
2026-07-28 18:12:49 +0800

    fix(tres): allow five minute strict wake arming

--- board/drivers/drivers.h
+++ board/drivers/drivers.h
@@ -63,7 +63,7 @@ extern volatile bool wake_monitor_som_off_seen;
 extern volatile bool wake_monitor_som_off_ready;
 extern volatile uint8_t wake_monitor_som_off_countdown;
 extern volatile bool wake_monitor_can_armed;
-extern volatile uint8_t wake_monitor_can_baseline_countdown;
+extern volatile uint16_t wake_monitor_can_baseline_countdown;
 extern volatile int8_t wake_monitor_tesla_counter;
 extern volatile int8_t wake_monitor_tesla_door_counter;
 extern volatile uint8_t wake_monitor_tesla_front_door_known_mask;
